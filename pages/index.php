01e6ccda9fdbf03a9d3cbb21644cdd665cf12e48
anhtu4156 <anhtu4156@gmail.com>
2023-07-19 23:40:56 +0700

    Khởi tạo các file chứa

---
+++ pages/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web phụ kiện điện thoại</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+            
+        </div>
+        <div class="Menu">
+            <p>menu</p>
+        </div>
+        <div class="Banner">
+            <p>banner</p>
+        </div>
+        <div class="Footer">
+            <p>footer</p>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
